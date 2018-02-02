13fd01024b421524268de1b5cf7cd348cbfb458b
Unknown <randybruno424@gmail.com>
2018-02-02 09:29:17 -0500

    Repo

---
+++ app/src/main/java/edu/fsu/cs/mobile/hw3/MyWebFragment.java
@@ -0,0 +1,72 @@
+package edu.fsu.cs.mobile.hw3;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+
+
+public class MyWebFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public MyWebFragment() {
+        // Required empty public constructor
+    }
+
+    public static MyWebFragment newInstance(String param1, String param2) {
+        MyWebFragment fragment = new MyWebFragment();
+        Bundle args = new Bundle();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_web, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
